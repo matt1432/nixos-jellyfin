9bcebf0d6e8cd20d6860879dcba30efb6741d398
matt1432 <matt@nelim.org>
2024-03-04 08:31:32 -0500

    fix: link services to restart together

--- modules/default.nix
+++ modules/default.nix
@@ -41,6 +41,8 @@ in {
   };
 
   config = mkIf (cfg.enable && cfg.settings != null) {
+    systemd.services."jellyfin".partOf = ["jellyfin-conf.service"];
+
     systemd.services."jellyfin-conf" = let
       jellyConfig = config.systemd.services.jellyfin.serviceConfig;
       configDir = "${jellyConfig.WorkingDirectory}/config";
@@ -52,6 +54,7 @@ in {
       wantedBy = ["multi-user.target"];
       before = ["jellyfin.service"];
       requiredBy = ["jellyfin.service"];
+      partOf = ["jellyfin.service"];
 
       serviceConfig = {
         User = cfg.user;
