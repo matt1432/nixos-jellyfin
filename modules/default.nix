366d23b761f2e41dcd9c5573413e7b7e6031504a
matt1432 <matt@nelim.org>
2024-03-04 12:19:51 -0500

    fix: make listOf enum

--- modules/default.nix
+++ modules/default.nix
@@ -186,11 +186,11 @@ in {
                 default = true;
               };
               hardwareDecodingCodecs = mkOption {
-                type = types.enum ["h264" "hevc" "mpeg2video" "mpeg4" "vc1" "vp8" "vp9" "avi"];
+                type = with types; listOf (enum ["h264" "hevc" "mpeg2video" "mpeg4" "vc1" "vp8" "vp9" "avi"]);
                 default = ["h264" "vc1"];
               };
               allowOnDemandMetadataBasedKeyframeExtractionForExtensions = mkOption {
-                type = types.enum ["mkv"];
+                type = with types; listOf (enum ["mkv"]);
                 default = ["mkv"];
               };
             };
