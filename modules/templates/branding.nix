c6a2d2ba3353b6d2fe669876c01bb044a00d184e
matt1432 <matt@nelim.org>
2024-03-04 08:22:14 -0500

    fix: correct template values

--- modules/templates/branding.nix
+++ modules/templates/branding.nix
@@ -1,12 +1,12 @@
-{cfg, ...}:
+{cfg, lib,  ...}:
 /*
 xml
 */
 ''
   <?xml version="1.0" encoding="utf-8"?>
   <BrandingOptions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-    <LoginDisclaimer${cfg.loginDisclaimer}/>
-    <CustomCss${cfg.customCss}/>
-    <SplashscreenEnabled>${toString cfg.splashscreenEnabled}</SplashscreenEnabled>
+    <LoginDisclaimer>${cfg.loginDisclaimer}</LoginDisclaimer>
+    <CustomCss>${cfg.customCss}</CustomCss>
+    <SplashscreenEnabled>${lib.boolToString cfg.splashscreenEnabled}</SplashscreenEnabled>
   </BrandingOptions>
 ''
