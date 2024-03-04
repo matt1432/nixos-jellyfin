1ab83e74e6911318f8dd6e563ef43079ed0acae5
matt1432 <matt@nelim.org>
2024-03-03 22:02:10 -0500

    feat: add branding options

--- modules/default.nix
+++ modules/default.nix
@@ -1,1 +1,43 @@
-{...}: {}
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mdDocs mkIf mkOption types;
+
+  cfg = config.services.jellyfin;
+in {
+  options.services.jellyfin = {
+    settings = mkOption {
+      default = null;
+      type = with types;
+        nullOr (submodule {
+          options = {
+            general = {
+              branding = {
+                loginDisclaimer = mkOption {
+                  type = types.str;
+                  default = " ";
+                  description = mdDocs "A message that will be displayed at the bottom of the login page.";
+                };
+
+                customCss = mkOption {
+                  type = types.lines;
+                  default = " ";
+                  description = mdDocs "Apply your custom CSS code for theming/branding on the web interface.";
+                };
+
+                splashscreenEnabled = mkOption {
+                  type = types.bool;
+                  default = false;
+                  description = mdDocs "Enable the splash screen";
+                };
+              };
+            };
+          };
+        });
+    };
+  };
+
+  config = mkIf (cfg.enable && cfg.settings != null) {};
+}
---
+++ modules/templates/branding.nix
@@ -0,0 +1,12 @@
+{cfg, ...}:
+/*
+xml
+*/
+''
+  <?xml version="1.0" encoding="utf-8"?>
+  <BrandingOptions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <LoginDisclaimer${cfg.loginDisclaimer}/>
+    <CustomCss${cfg.customCss}/>
+    <SplashscreenEnabled>${cfg.splashscreenEnabled}</SplashscreenEnabled>
+  </BrandingOptions>
+''
