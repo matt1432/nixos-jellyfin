708f31e8b52587a14689052758909cff1e201bac
Updater <robot@nowhere.invalid>
2024-06-25 01:09:10 +0000

    ci: jellyfin v10.9.6 -> v10.9.7

--- pkgs/jellyfin/src.nix
+++ pkgs/jellyfin/src.nix
@@ -2,6 +2,6 @@
 {
   owner = "jellyfin";
   repo = "jellyfin";
-  rev = "v10.9.6";
-  hash = "sha256-Ze1KO+Rx4sz8qdKa1U2g096Ck/Qc+JdQj4MNqaJRhb8=";
+  rev = "v10.9.7";
+  hash = "sha256-jxOFbmYrgxP6jbjnWubajqXInLXu7TO4vssU4E1oeoc=";
 }
