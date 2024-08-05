35a755c3fb2836623d04af70cc7972d0f366370d
matt1432 <matt@nelim.org>
2024-08-05 11:41:57 -0400

    fix(jellyfin-server): ignore error SA1201

--- pkgs/jellyfin/default.nix
+++ pkgs/jellyfin/default.nix
@@ -16,6 +16,10 @@ in
 
     src = fetchFromGitHub jellyfin-src;
 
+    postPatch = ''
+      sed -i '1s/^/#pragma warning disable SA1201\n/' ./MediaBrowser.MediaEncoding/Encoder/EncoderValidator.cs
+    '';
+
     propagatedBuildInputs = [
       sqlite
     ];
