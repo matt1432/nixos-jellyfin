f1783a66b4b2c5fc2ae8baa8b7446cdd9f41b114
matt1432 <matt@nelim.org>
2024-10-26 18:04:35 -0400

    fix(ffmpeg): switch to ffmpeg7

--- pkgs/jellyfin-ffmpeg/default.nix
+++ pkgs/jellyfin-ffmpeg/default.nix
@@ -1,15 +1,13 @@
 {
-  chromaprint,
   fetchFromGitHub,
-  fetchpatch,
-  ffmpeg_6-full,
+  ffmpeg_7-full,
   lib,
 }: let
   jellyfin-ffmpeg-src = import ./src.nix;
 
   version = lib.removePrefix "v" jellyfin-ffmpeg-src.rev;
 in
-  (ffmpeg_6-full.override {
+  (ffmpeg_7-full.override {
     inherit version; # Important! This sets the ABI.
     source = fetchFromGitHub jellyfin-ffmpeg-src;
   })
@@ -17,23 +15,7 @@ in
     pname = "jellyfin-ffmpeg";
 
     # Clobber upstream patches as they don't apply to the Jellyfin fork
-    patches = [
-      (fetchpatch {
-        name = "fix_build_failure_due_to_libjxl_version_to_new";
-        url = "https://git.ffmpeg.org/gitweb/ffmpeg.git/patch/75b1a555a70c178a9166629e43ec2f6250219eb2";
-        hash = "sha256-+2kzfPJf5piim+DqEgDuVEEX5HLwRsxq0dWONJ4ACrU=";
-      })
-    ];
-
-    buildInputs = old.buildInputs ++ [chromaprint];
-
-    configureFlags =
-      old.configureFlags
-      ++ [
-        "--extra-version=Jellyfin"
-        "--disable-ptx-compression" # https://github.com/jellyfin/jellyfin/issues/7944#issuecomment-1156880067
-        "--enable-chromaprint"
-      ];
+    patches = [];
 
     postPatch = ''
       for file in $(cat debian/patches/series); do
